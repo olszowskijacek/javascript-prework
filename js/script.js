092d0820b3d14efca8b742d11db3dd1020739f44
Jacek Olszowski <jacek.olszowski1@gmail.com>
2019-08-01 17:30:46 +0200

    game result

--- js/script.js
+++ js/script.js
@@ -6,12 +6,11 @@ let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
-} else if (randomNumber == 2){
-    computerMove = 'papier';
-} else if (randomNumber == 3){
-    computerMove = 'nożyce';
+} else if(randomNumber == 2){
+ 	computerMove = 'papier'; 
+} else if(randomNumber == 3){
+ 	computerMove = 'nożyce'; 
 }
-  
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -23,10 +22,36 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-} else if (playerInput == 2){
-    playerMove = 'papier';
-} else if (playerInput =='3'){
-    playerMove = 'nożyce';
+} else if(playerInput == '2'){
+  playerMove = 'papier';
+} else if(playerInput == '3'){
+	playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'nożyce' &&playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}  else if(computerMove == 'kamień' && playerMove =='kamień'){
+    printMessage('Remis');
+}   else if(computerMove == 'papier' && playerMove =='papier'){
+    printMessage('Remis');
+}   else if(computerMove == 'nożyce' && playerMove =='nożyce'){
+    printMessage('Remis');
+}   else if(computerMove == 'kamień' && playerMove =='nieznany ruch'){
+    printMessage('Nie oszukuj!');
+}   else if(computerMove == 'papier' && playerMove =='nieznany ruch'){
+    printMessage('Nie oszukuj!');
+}   else if(computerMove == 'nożyce' && playerMove =='nieznany ruch'){
+    printMessage('Nie oszukuj!');
+}   else {
+    printMessage('Przegrałeś :(');
+}
+
+
